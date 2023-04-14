88b8b090b6a02f7341ce5fe2987c74eea5b37db3
JohanGoutorbe <contact@johan-goutorbe.fr>
2023-04-14 11:32:13 +0200

    Db Connect test : ok

--- index.php
+++ index.php
@@ -5,4 +5,14 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
+include './dbconnect.php';
+
+$query  = "SELECT * FROM `test`";
+$stmt = $db->prepare($query);
+$stmt->execute();
+while ($query = $stmt->fetch()) {
+    echo $query['test'] . "</br>";
+}
+
+
 ?>
