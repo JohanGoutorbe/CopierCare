c3e947a0e4ae433970219fc1756131ced32a9a52
JohanGoutorbe <contact@johan-goutorbe.fr>
2023-04-14 15:26:15 +0200

    Update accueil.php

--- accueil.php
+++ accueil.php
@@ -1,4 +1,51 @@
 <body>
-    
+    <div class="container">
+        <aside>
+            <div class="top">
+                <div class="logo">
+                    <img src="./files/logo_copiercare.png" alt="CopierCare logo">
+                    <h2><span class="danger">COPIER</span>CARE</h2>
+                </div>
+                <div class="close" id="close-btn">
+                    <span class="material-icons-sharp">close</span>
+                </div>
+            </div>
+
+            <div class="sidebar">
+                <a href="#">
+                    <span class="material-icons-sharp">home</span>
+                    <h3>Accueil</h3>
+                </a>
+                <a href="#">
+                    <span class="material-icons-sharp">report_gmailerrorred</span>
+                    <h3>Alertes</h3>
+                </a>
+                <a href="#">
+                    <span class="material-icons-sharp">groups</span>
+                    <h3>Clients</h3>
+                </a>
+                <a href="#">
+                    <span class="material-icons-sharp">print_outline</span>
+                    <h3>Copieurs</h3>
+                </a>
+                <a href="#">
+                    <span class="material-icons-sharp">construction</span>
+                    <h3>Pièces</h3>
+                </a>
+                <a href="#">
+                    <span class="material-icons-sharp">settings</span>
+                    <h3>Paramètres</h3>
+                </a>
+                <a href="#">
+                    <span class="material-icons-sharp">admin_panel_settings</span>
+                    <h3>Administrateur</h3>
+                </a>
+                <a href="#">
+                    <span class="material-icons-sharp">logout</span>
+                    <h3>Se déconnecter</h3>
+                </a>
+            </div>
+        </aside>
+    </div>
 </body>
 </html>
