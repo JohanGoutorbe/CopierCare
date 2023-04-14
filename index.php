c3fd30092563e64e72a766151f9765f91e52c0e1
JohanGoutorbe <contact@johan-goutorbe.fr>
2023-04-14 14:07:54 +0200

    Update index.php

--- index.php
+++ index.php
@@ -10,8 +10,20 @@ include './dbconnect.php';
 $query  = "SELECT * FROM `test`";
 $stmt = $db->prepare($query);
 $stmt->execute();
+
+$iterations_number = 3;
+$longueur_releve_compteur = 7;
+
 while ($query = $stmt->fetch()) {
-    echo $query['test'] . "</br>";
+    $i = 1;
+    $values = -9;
+    while ($i-1 < $iterations_number) {
+        $actual_values = $values * $i;
+        $num = substr($query['test'], $actual_values, $longueur_releve_compteur);
+        $i++;
+        echo $num . "; ";
+    }
+    echo "</br>";
 }
 
 
