1981fb147ee312edb82e076a21d0810d3b5508e6
JohanGoutorbe <contact@johan-goutorbe.fr>
2023-04-14 14:26:56 +0200

    Update index.php

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
 include './dbconnect.php';
-
+/*
 $query  = "SELECT * FROM `test`";
 $stmt = $db->prepare($query);
 $stmt->execute();
@@ -25,6 +25,7 @@ while ($query = $stmt->fetch()) {
     }
     echo "</br>";
 }
+*/
 
-
+include './head.php';
 ?>
