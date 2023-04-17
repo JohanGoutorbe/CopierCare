17f9327f996164c60258275caa46268fd7b5552a
JohanGoutorbe <contact@johan-goutorbe.fr>
2023-04-17 15:53:05 +0200

    Update login.php

--- login.php
+++ login.php
@@ -14,11 +14,18 @@ error_reporting(E_ALL);
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>CopierCare | Connexion</title>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons+Sharp">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons+outlined">
+    <link rel="stylesheet" href="./style/login_style.css">
     <link rel="shortcut icon" href="./files/logo_copiercare.png" type="image/x-icon">
 </head>
 
 <body>
-
+    <div class="container">
+        <form action="" method="post">
+            <h1>Se connecter</h1>
+        </form>
+    </div>
 </body>
 
 </html>
