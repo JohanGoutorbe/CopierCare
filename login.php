01f7af8d90b52ab1b04cc7a580c14c73e21fcbc2
JohanGoutorbe <contact@johan-goutorbe.fr>
2023-04-17 16:13:51 +0200

    Update login.php

--- login.php
+++ login.php
@@ -14,18 +14,29 @@ error_reporting(E_ALL);
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>CopierCare | Connexion</title>
-    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons+Sharp">
-    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons+outlined">
     <link rel="stylesheet" href="./style/login_style.css">
     <link rel="shortcut icon" href="./files/logo_copiercare.png" type="image/x-icon">
 </head>
 
 <body>
-    <div class="container">
-        <form action="" method="post">
+    <section>
+        <form action="./login_processing.php" method="post">
             <h1>Se connecter</h1>
+            <p class="choose-email">En utilisant un nom d'utilisateur :</p>
+            <div class="inputs">
+                <input type="text" placeholder="Nom d'utiliateur">
+                <input type="password" placeholder="Mot de passe">
+                <button type="submit">Se connecter</button>
+            </div>
         </form>
-    </div>
+        <form action="./login_processing.php" method="post">
+            <p class="choose-email">En utilisant un nom d'utilisateur :</p>
+            <div class="inputs">
+                <input type="text" placeholder="Nom d'utiliateur">
+                <input type="password" placeholder="Mot de passe">
+                <button type="submit">Se connecter</button>
+            </div>
+        </form>
+    </section>
 </body>
-
 </html>
