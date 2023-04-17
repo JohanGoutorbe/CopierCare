f470fd8c8934016944982bb7ad33c72834fdd570
JohanGoutorbe <contact@johan-goutorbe.fr>
2023-04-17 15:50:53 +0200

    Update login.php

--- login.php
+++ login.php
@@ -4,7 +4,21 @@
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
-
-include './head.php';
 ?>
 
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CopierCare | Connexion</title>
+    <link rel="shortcut icon" href="./files/logo_copiercare.png" type="image/x-icon">
+</head>
+
+<body>
+
+</body>
+
+</html>
