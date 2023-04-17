80b473bc1f50c8c1a7d14911feb1acfcc1358027
JohanGoutorbe <contact@johan-goutorbe.fr>
2023-04-17 11:22:56 +0200

    Update accueil.php

--- accueil.php
+++ accueil.php
@@ -140,7 +140,7 @@
                     </div>
                 </div>
             </div>
-            <!-- END OF TOP -->
+            <!--------------- END OF TOP ------------>
             <div class="recent-updates">
                 <h2>Recent Updates</h2>
                 <div class="updates">
@@ -173,8 +173,57 @@
                     </div>
                 </div>
             </div>
+            <!--------------- END OF RECENT UPDATES ------------>
+            <div class="sales-analytics">
+                <h2>Sales Analytics</h2>
+                <div class="item online">
+                    <div class="icon">
+                        <span class="material-icons-sharp">shopping_cart</span>
+                    </div>
+                    <div class="right">
+                        <div class="info">
+                            <h3>ONLINE ORDERS</h3>
+                            <small class="text-muted">LAst 24 hours</small>
+                        </div>
+                        <h5 class="success">+39%</h5>
+                        <h3>3849</h3>
+                    </div>
+                </div>
+                <div class="item offline">
+                    <div class="icon">
+                        <span class="material-icons-sharp">local_mall</span>
+                    </div>
+                    <div class="right">
+                        <div class="info">
+                            <h3>OFFLINE ORDERS</h3>
+                            <small class="text-muted">LAst 24 hours</small>
+                        </div>
+                        <h5 class="danger">-17%</h5>
+                        <h3>1100</h3>
+                    </div>
+                </div>
+                <div class="item customers">
+                    <div class="icon">
+                        <span class="material-icons-sharp">person</span>
+                    </div>
+                    <div class="right">
+                        <div class="info">
+                            <h3>NEW CUSTOMERS</h3>
+                            <small class="text-muted">LAst 24 hours</small>
+                        </div>
+                        <h5 class="success">+25%</h5>
+                        <h3>849</h3>
+                    </div>
+                </div>
+                <div class="item add-product">
+                    <div>
+                        <span class="material-icons-sharp">add</span>
+                        <h3>Add Product</h3>
+                    </div>
+                </div>
+            </div>
         </div>
-    </div>
-</body>
+        </div>
+        </body>
 
-</html>
+        </html>
