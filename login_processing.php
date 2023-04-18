17a06df16e8086decf0575fdaf09f0aea415dcba
JohanGoutorbe <contact@johan-goutorbe.fr>
2023-04-18 12:37:06 +0200

    Update login_processing.php

--- login_processing.php
+++ login_processing.php
@@ -1,3 +1,30 @@
 <?php
 
-session_start();
+// Affichage des erreurs
+ini_set('display_errors', 1);
+ini_set('displau_startup_errors', 1);
+error_reporting(E_ALL);
+
+// Connexion à la base de données
+include './dbconnect.php';
+
+if (isset($_POST['username']) && isset($_POST['pwd'])) {
+    if (!empty($_POST['username']) && !empty($_POST['pwd'])) {
+        $username = htmlspecialchars($_POST['username']);
+        $pwd = htmlspecialchars($_POST['pwd']);
+    } else {
+        $_SESSION['errors'] = "L'identifiant ou le mot de passe est vide";
+        header('Location: login.php');
+    }
+} elseif (isset($_POST['email']) && isset($_POST['pwd'])) {
+    if (!empty($_POST['email']) && !empty($_POST['pwd'])) {
+        $email = htmlspecialchars($_POST['email']);
+        $pwd = htmlspecialchars($_POST['pwd']);
+    } else {
+        $_SESSION['errors'] = "L'adresse mail ou le mot de passe est vide";
+        header('Location: login.php');
+    }
+} else {
+    $_SESSION['errors'] = "Veuillez vous connecter avec l'une des méthodes proposées ci-dessus.";
+    header('Location: login.php');
+}
