6ea3bba4dce3b96cb900cd781cc02151c5198789
JohanGoutorbe <contact@johan-goutorbe.fr>
2023-04-19 16:00:13 +0200

    temporary sql file

---
+++ temp.sql
@@ -0,0 +1,14 @@
+INSERT INTO `pieces`(`nom`, `ref`) VALUES ('Tambour Noir','tamN');
+INSERT INTO `pieces`(`nom`, `ref`) VALUES ('Tambour Cyan','tamC');
+INSERT INTO `pieces`(`nom`, `ref`) VALUES ('ambour Jaune','tamY');
+INSERT INTO `pieces`(`nom`, `ref`) VALUES ('Tambour Magenta','tamM');
+INSERT INTO `pieces`(`nom`, `ref`) VALUES ('Développeur Noir','devN');
+INSERT INTO `pieces`(`nom`, `ref`) VALUES ('Développeur Cyan','devC');
+INSERT INTO `pieces`(`nom`, `ref`) VALUES ('Développeur Jaune','devY');
+INSERT INTO `pieces`(`nom`, `ref`) VALUES ('Développeur Magenta','devM');
+INSERT INTO `pieces`(`nom`, `ref`) VALUES ('Courroie de transfert','courtran');
+INSERT INTO `pieces`(`nom`, `ref`) VALUES ('Rouleau de transfert secondaire','roul2');
+INSERT INTO `pieces`(`nom`, `ref`) VALUES ('Four','four');
+INSERT INTO `pieces`(`nom`, `ref`) VALUES ('Patin cassette 1','pat1');
+INSERT INTO `pieces`(`nom`, `ref`) VALUES ('Patin cassette 2','pat2');
+INSERT INTO `pieces`(`nom`, `ref`) VALUES ('Patin chargeur','patchar')
