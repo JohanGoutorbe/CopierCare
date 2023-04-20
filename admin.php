8ad456b31483f75ecc5bcff57d0fe343bc00041c
JohanGoutorbe <contact@johan-goutorbe.fr>
2023-04-20 16:06:20 +0200

    Update admin.php

--- admin.php
+++ admin.php
@@ -7,6 +7,15 @@ error_reporting(E_ALL);
 
 // Connexion à la base de données
 include './dbconnect.php';
+
+if (@isset($_SESSION['logged']) && @$_SESSION['logged'] == true) {
+    $_SESSION['validate'] = true;
+} else {
+    $_SESSION['errors'] = "Veuillez vous authentifier à l'aide de l'une des méthodes ci-dessus.";
+    header('Location: login.php');
+    exit();
+}
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
