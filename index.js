65034447f19ef0793af5ebcaab6103fd9fa9b830
JohanGoutorbe <contact@johan-goutorbe.fr>
2023-04-20 17:22:50 +0200

    Update index.js

--- index.js
+++ index.js
@@ -16,6 +16,7 @@ closeBtn.addEventListener('click', () => {
 // Change theme
 themeToggler.addEventListener('click', () => {
     document.body.classList.toggle('dark-theme-variables');
+    sessionStorage.setItem('theme', 'black');
 
     themeToggler.querySelector('span:nth-child(1)').classList.toggle('active');
     themeToggler.querySelector('span:nth-child(2)').classList.toggle('active');
