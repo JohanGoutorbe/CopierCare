d3551ba55df9b3905feddca687183342fc77beaf
JohanGoutorbe <contact@johan-goutorbe.fr>
2023-04-20 15:42:04 +0200

    Update logout.php

---
+++ logout.php
@@ -0,0 +1,21 @@
+<?php
+
+// Réinitialisation des variables de session
+$_SESSION['logged'] = false;
+$_SESSION['username'] = "";
+$_SESSION['surname'] = "";
+$_SESSION['photo'] = "";
+$_SESSION['rang'] = "";
+
+// Suppression totale des variables de sessions
+session_unset();
+
+// Fermeture complète de la session
+session_destroy();
+
+// Redirection vers le formulaire de connexion
+header('location: login.php');
+
+exit;
+
+?>
