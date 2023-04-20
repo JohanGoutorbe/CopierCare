21c86a842b0a12b018da389855dce5e597dbbad5
JohanGoutorbe <contact@johan-goutorbe.fr>
2023-04-20 16:34:37 +0200

    Update admin.php

--- admin.php
+++ admin.php
@@ -66,6 +66,10 @@ if (@isset($_SESSION['logged']) && @$_SESSION['logged'] == true) {
                     <span class="material-icons-sharp">construction</span>
                     <h3>Consommables</h3>
                 </a>
+                <a href="./pieces.php">
+                    <span class="material-icons-sharp">devices</span>
+                    <h3>Pièces</h3>
+                </a>
                 <a href="./parametres.php">
                     <span class="material-icons-sharp">settings</span>
                     <h3>Paramètres</h3>
