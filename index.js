975a927a366a943ec03efd2f5aa2685f4f694aa0
JohanGoutorbe <contact@johan-goutorbe.fr>
2023-04-21 16:37:36 +0200

    Update index.js

--- index.js
+++ index.js
@@ -13,11 +13,26 @@ closeBtn.addEventListener('click', () => {
     sideMenu.style.display = 'none';
 })
 
+// themeToggler.addEventListener('load', () => {
+//     let sessionthemed = sessionStorage.getItem('theme');
+//     if (sessionthemed = 'black') {
+//         themeToggler.querySelector('span:nth-child(1)').classList.toggle('active');
+//         themeToggler.querySelector('span:nth-child(2)').classList.toggle('active');
+//     }
+// })
+
 // Change theme
 themeToggler.addEventListener('click', () => {
     document.body.classList.toggle('dark-theme-variables');
-    sessionStorage.setItem('theme', 'black');
-
+    
     themeToggler.querySelector('span:nth-child(1)').classList.toggle('active');
     themeToggler.querySelector('span:nth-child(2)').classList.toggle('active');
+
+    // let sessionthemed = sessionStorage.getItem('theme');
+    // if (sessionthemed =  'white') {
+    //     sessionStorage.setItem('theme', 'black');
+    // }
+    // if (sessionthemed = 'black') {
+    //     sessionStorage.setItem('theme', 'white');
+    // }
 })
