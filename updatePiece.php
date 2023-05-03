c7546ebdc520371e1a7919650f62f71a028bd613
JohanGoutorbe <contact@johan-goutorbe.fr>
2023-05-03 11:29:11 +0200

    big update of updatePiece.php

---
+++ updatePiece.php
@@ -0,0 +1,65 @@
+<?php
+
+//Affichage des erreurs sur la page web
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+// Connexion à la base de données
+include './dbconnect.php';
+
+include './loggedVerif.php';
+
+$error = '<p style="color: #ff7782;">L\'url est incorrecte</p>';
+
+
+$_SESSION['message'] = '<p style="color: #ff7782;">Erreur de modification de la pièce</p><br>';
+
+
+if (!isset($_POST[('pieceUpdateSubmit')])) {
+    $_SESSION['message'] .= '<p style="color: #ff7782;">L\'envoi du formulaire a échoué</p>';
+    header('Location: piece.php');
+    exit();
+}
+
+$id = htmlspecialchars(empty($_POST['id']));
+$name = htmlspecialchars(empty($_POST['name']));
+$ref = htmlspecialchars(empty($_POST['ref']));
+
+if (empty($id)) {
+    $_SESSION['message'] .= '<p style="color: #ff7782;">L\'id est vide</p>';
+    header('Location: piece.php');
+    exit();
+} elseif (empty($_POST['name'])) {
+    $_SESSION['message'] .= '<p style="color: #ff7782;">Le nom de la pièce est vide</p>';
+    header('Location: piece.php');
+    exit();
+} elseif (empty($_POST['ref'])) {
+    $_SESSION['message'] .= '<p style="color: #ff7782;">La référence de la pièce est vide</p>';
+    header('Location: piece.php');
+    exit();
+}
+
+if (strlen($name) > 100) {
+    $_SESSION['message'] .= '<p style="color: #ff7782;">Le nom de la pièce excède 100 caractères</p>';
+    header('Location: piece.php');
+    exit();
+} elseif (strlen($ref) > 20) {
+    $_SESSION['message'] .= '<p style="color: #ff7782;">La référence de la pièce excède 20 caractères</p>';
+    header('Location: piece.php');
+    exit();
+} elseif (strlen($name) < strlen($ref)) {
+    $_SESSION['message'] .= '<p style="color: #ff7782;">La référence de la pièce doit être plus courte que son nom</p>';
+    header('Location: piece.php');
+    exit();
+}
+
+
+
+$_SESSION['message'] = "";
+$sql = "INSERT INTO `pieces` (`nom`, `ref`) VALUES (:name, :ref)";
+$stmt = $db->prepare($sql);
+$stmt->bindParam('name', $pieceName);
+$stmt->bindParam('ref', $pieceRef);
+$stmt->execute();
+$_SESSION['message'] = '<p style="color: #41f1b6; text-shadow: 0px 0px black;">La pièce <strong>' . $name . '</strong> a bien été ajoutée.</p>';
