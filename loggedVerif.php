9d53459abcb7fd74244f156dc37a38fdfe6005c8
JohanGoutorbe <contact@johan-goutorbe.fr>
2023-05-03 17:35:30 +0200

    Update loggedVerif.php

--- loggedVerif.php
+++ loggedVerif.php
@@ -1,6 +1,6 @@
 <?php
 
-if (isset($_SESSION['logged']) && $_SESSION['logged'] == true) {
+if (isset($_SESSION['logged']) && $_SESSION['logged'] == true && !empty($_SESSION['username'])) {
     $_SESSION['validate'] = true;
 } else {
     $_SESSION['errors'] = "Veuillez vous authentifier à l'aide de l'une des méthodes ci-dessus.";
