10ea2b3bef3e91a965642cfea0f5010fd24d9e06
JohanGoutorbe <contact@johan-goutorbe.fr>
2023-05-05 08:38:57 +0200

    Update deletePiece.php

--- deletePiece.php
+++ deletePiece.php
@@ -28,18 +28,35 @@ if (!isset($_GET['id'])) {
     $_SESSION['message'] = $error;
     header($location);
     exit();
+} elseif (!ctype_digit($_GET['id'])) {
+    $_SESSION['message'] = $error;
+    header($location);
+    exit();
 }
-i
+if (!isset($_GET['name'])) {
+    $_SESSION['message'] = $error;
+    header($location);
+    exit();
+} elseif (empty($_GET['name'])) {
+    $_SESSION['message'] = $error;
+    header($location);
+    exit();
+} elseif (!ctype_alpha($_GET['name'])) {
+    $_SESSION['message'] = $error;
+    header($location);
+    exit();
+}
 
 $table = trim(htmlspecialchars($_GET['table']));
 $id = htmlspecialchars($_GET['id']);
+$name = htmlspecialchars($_GET['name']);
 
 $sql = "DELETE FROM `" . $table . "` WHERE `id` = :id";
 $stmt = $db->prepare($sql);
 $stmt->bindParam('id', $id);
 $stmt->execute();
 
-$_SESSION['message'] = '<p style="color: #41f1b6; font-size: 1.25em; font-weight: 100; width: 100vw;">La suppression s\'est correctement effectuée</p>';
+$_SESSION['message'] = '<p style="color: #41f1b6; font-size: 1.25em; font-weight: 100;">La suppression de la pièce <strong>' . $name . '</strong> s\'est effectuée</p>';
 
 $location = 'Location: ' . $table . '.php';
 header($location);
