d3efb442b8cc115a40cc095df99cd19217e879d2
JohanGoutorbe <contact@johan-goutorbe.fr>
2023-05-10 11:16:13 +0200

    Update updateUser.php

--- admin/updateUser.php
+++ admin/updateUser.php
@@ -13,11 +13,11 @@ include '../utils/loggedVerif.php';
 $error = '<p style="color: #ff7782;">L\'url est incorrecte</p>';
 
 
-$_SESSION['message'] = '<p style="color: #ff7782;">Erreur de modification de l\'utilisateur</p><br>';
+$_SESSION['messageAdmin'] = '<p style="color: #ff7782;">Erreur de modification de l\'utilisateur</p><br>';
 
 
 if (!isset($_POST[('userUpdateSubmit')])) {
-    $_SESSION['message'] .= '<p style="color: #ff7782;">L\'envoi du formulaire a échoué</p>';
+    $_SESSION['messageAdmin'] .= '<p style="color: #ff7782;">L\'envoi du formulaire a échoué</p>';
     header('Location: ./admin.php');
     exit();
 }
@@ -30,53 +30,53 @@ $rang = htmlspecialchars($_POST['rang']);
 $formation = htmlspecialchars($_POST['formation']);
 
 if (empty($id)) {
-    $_SESSION['message'] .= '<p style="color: #ff7782;">L\'id est vide</p>';
+    $_SESSION['messageAdmin'] .= '<p style="color: #ff7782;">L\'id est vide</p>';
     header('Location: ./admin.php');
     exit();
 } elseif (empty($nom)) {
-    $_SESSION['message'] .= '<p style="color: #ff7782;">Le nom de l\'utilisateur est vide</p>';
+    $_SESSION['messageAdmin'] .= '<p style="color: #ff7782;">Le nom de l\'utilisateur est vide</p>';
     header('Location: ./admin.php');
     exit();
 } elseif (empty($prenom)) {
-    $_SESSION['message'] .= '<p style="color: #ff7782;">La référence de l\'utilisateur est vide</p>';
+    $_SESSION['messageAdmin'] .= '<p style="color: #ff7782;">La référence de l\'utilisateur est vide</p>';
     header('Location: ./admin.php');
     exit();
 } elseif (empty($email)) {
-    $_SESSION['message'] .= '<p style="color: #ff7782;">L\'email de l\'utilisateur est vide</p>';
+    $_SESSION['messageAdmin'] .= '<p style="color: #ff7782;">L\'email de l\'utilisateur est vide</p>';
     header('Location: ./admin.php');
     exit();
 } elseif (empty($rang)) {
-    $_SESSION['message'] .= '<p style="color: #ff7782;">Le rang de l\'utilisateur est vide</p>';
+    $_SESSION['messageAdmin'] .= '<p style="color: #ff7782;">Le rang de l\'utilisateur est vide</p>';
     header('Location: ./admin.php');
     exit();
 }
 
 if (!ctype_digit($id)) {
-    $_SESSION['message'] .= '<p style="color: #ff7782;">L\'id de l\'utilisateur est incorrect</p>';
+    $_SESSION['messageAdmin'] .= '<p style="color: #ff7782;">L\'id de l\'utilisateur est incorrect</p>';
     header('Location: ./admin.php');
     exit();
 }
 
 if (strlen($nom) > 50) {
-    $_SESSION['message'] .= '<p style="color: #ff7782;">Le nom de l\'utilisateur excède 50 caractères</p>';
+    $_SESSION['messageAdmin'] .= '<p style="color: #ff7782;">Le nom de l\'utilisateur excède 50 caractères</p>';
     header('Location: ./admin.php');
     exit();
 } elseif (strlen($prenom) > 50) {
-    $_SESSION['message'] .= '<p style="color: #ff7782;">Le prénom de l\'utilisateur excède 50 caractères</p>';
+    $_SESSION['messageAdmin'] .= '<p style="color: #ff7782;">Le prénom de l\'utilisateur excède 50 caractères</p>';
     header('Location: ./admin.php');
     exit();
 } elseif (strlen($email) > 100) {
-    $_SESSION['message'] .= '<p style="color: #ff7782;">L\'email de l\'utilisateur excède 50 caractères</p>';
+    $_SESSION['messageAdmin'] .= '<p style="color: #ff7782;">L\'email de l\'utilisateur excède 50 caractères</p>';
     header('Location: ./admin.php');
     exit();
 } elseif (strlen($rang) > 50) {
-    $_SESSION['message'] .= '<p style="color: #ff7782;">Le rang de l\'utilisateur excède 50 caractères</p>';
+    $_SESSION['messageAdmin'] .= '<p style="color: #ff7782;">Le rang de l\'utilisateur excède 50 caractères</p>';
     header('Location: ./admin.php');
     exit();
 } 
 
 if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-    $_SESSION['message'] .= '<p style="color: #ff7782;">L\'email de l\'utilisateur est incorrect</p>';
+    $_SESSION['messageAdmin'] .= '<p style="color: #ff7782;">L\'email de l\'utilisateur est incorrect</p>';
     header('Location: ./admin.php');
     exit();
 }
@@ -90,6 +90,6 @@ $stmt->bindParam('rang', $rang);
 $stmt->bindParam('formation', $formation);
 $stmt->bindParam('id', $id);
 $stmt->execute();
-$_SESSION['message'] = '<p style="color: #41f1b6; text-shadow: 0px 0px black; font-size: 1.25em; font-weight: 100;">L\'utilisateur <strong>' . ucfirst($prenom) . ' ' . strtoupper($nom) . '</strong> a bien été modifié.</p>';
+$_SESSION['messageAdmin'] = '<p style="color: #41f1b6; text-shadow: 0px 0px black; font-size: 1.25em; font-weight: 100;">L\'utilisateur <strong>' . ucfirst($prenom) . ' ' . strtoupper($nom) . '</strong> a bien été modifié.</p>';
 header('Location: ./admin.php');
 exit();
