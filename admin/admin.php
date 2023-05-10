f3907730d3ec9c83b3a642bf9b385908718d88b2
JohanGoutorbe <contact@johan-goutorbe.fr>
2023-05-10 11:16:27 +0200

    Update admin.php

--- admin/admin.php
+++ admin/admin.php
@@ -14,6 +14,68 @@ $sql = "SELECT * FROM `utilisateurs` WHERE 1 ORDER BY id";
 $stmt = $db->prepare($sql);
 $stmt->execute();
 
+if (isset($_POST[('userSubmit')])) {
+    if (isset($_POST['userName']) || !empty($_POST['userName']) || strlen($_POST['userName']) <= 50) {
+        if (isset($_POST['userSurname']) || !empty($_POST['userSurname']) || strlen($_POST['userSurname']) <= 50) {
+            if (isset($_POST['userEmail']) || !empty($_POST['userEmail']) || strlen($_POST['userEmail']) <= 100 || filter_var($_POST['userEmail'], FILTER_VALIDATE_EMAIL)) {
+                if (isset($_POST['userRang']) || !empty($_POST['userRang']) || strlen($_POST['userRang']) <= 50 || ctype_alpha($_POST['userRang'])) {
+                    if (isset($_POST['userImage']) || !empty($_POST['userImage']) || isset($_FILES["userImage"]) || !empty($_FILES["userImage"])) {
+                        if (isset($_POST['userFormation'])) {
+                            if (isset($_POST['userLogin']) || !empty($_POST['userLogin'] || strlen($_POST['userLogin']) <= 30)) {
+                                if (isset($_POST['userPwd']) || !empty($_POST['userPwd']) || strlen($_POST['userPwd']) <= 50) {
+                                    // Insertion de l'image de profil dans la table 'images'
+                                    $sql = $db->prepare("insert into images(nom,taille,bin) values(?,?,?)");
+                                    $sql->execute(array($_FILES["userImage"]["name"], $_FILES["userImage"]["size"], file_get_contents($_FILES["userImage"]["tmp_name"])));
+
+                                    // Variables
+                                    $nom = strtolower(htmlspecialchars($_POST['userName']));
+                                    $prenom = strtolower(htmlspecialchars($_POST['userSurname']));
+                                    $email = htmlspecialchars($_POST['userEmail']);
+                                    $rang = strtolower(htmlspecialchars($_POST['userRang']));
+                                    $formation = htmlspecialchars($_POST['userFormation']);
+                                    $login = htmlspecialchars($_POST['userLogin']);
+                                    $pwd = htmlspecialchars($_POST['userPwd']);
+
+                                    // Insertion de l'utilisateur
+                                    $sql = "INSERT INTO `utilisateurs` (`nom`, `prenom`, `email`, `rang`, `photo`, `formation`, `identifiant`, `mdp`) VALUES (:nom, :prenom, :email, :rang, :photo, :formation, :identifiant, :mdp)";
+                                    $stmt = $db->prepare($sql);
+                                    $stmt->bindParam('nom', $nom);
+                                    $stmt->bindParam('prenom', $prenom);
+                                    $stmt->bindParam('email', $email);
+                                    $stmt->bindParam('rang', $rang);
+                                    $stmt->bindParam('photo', $_FILES["userImage"]["name"]);
+                                    $stmt->bindParam('formation', $formation);
+                                    $stmt->bindParam('identifiant', $login);
+                                    $stmt->bindParam('mdp', $pwd);
+                                    $stmt->execute();
+                                    $_SESSION['messageAdmin'] = '<p style="color: #41f1b6; text-shadow: 0px 0px black; width: auto;">L\'utilisateur <strong>' . ucfirst($prenom) . ' ' . strtoupper($nom) . '</strong> a bien été ajouté.</p>';
+                                    header("Refresh:0");
+                                } else {
+                                    $_SESSION['messageAdmin'] = '<p style="color: #ff7782;">Le mot de passe du technicien est incorrecte</p>';
+                                }
+                            } else {
+                                $_SESSION['messageAdmin'] = '<p style="color: #ff7782;">Le nom d\'utilisateur du technicien est incorrecte</p>';
+                            }
+                        } else {
+                            $_SESSION['messageAdmin'] = '<p style="color: #ff7782;">La formation associée au technicien est incorrecte</p>';
+                        }
+                    } else {
+                        $_SESSION['messageAdmin'] = '<p style="color: #ff7782;">L\'image associée au technicien est incorrecte</p>';
+                    }
+                } else {
+                    $_SESSION['messageAdmin'] = '<p style="color: #ff7782;">Le rang du technicien est incorrect</p>';
+                }
+            } else {
+                $_SESSION['messageAdmin'] = '<p style="color: #ff7782;">L\'adresse email du technicien est incorrecte</p>';
+            }
+        } else {
+            $_SESSION['messageAdmin'] = '<p style="color: #ff7782;">Le prénom du technicien est incorrect</p>';
+        }
+    } else {
+        $_SESSION['messageAdmin'] = '<p style="color: #ff7782;">Le nom du technicien est incorrect</p>';
+    }
+}
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -93,25 +155,49 @@ $stmt->execute();
             <h1>Administration du site</h1>
             <div class="alert">
                 <button id="AddUserBtn" class="AddPieceBtn">
-                    <h2>Ajouter une pièce</h2>
+                    <h2>Ajouter un utilisateur</h2>
                 </button>
                 <section class="sales" id="addPiece">
-                    <form action="./pieces.php" method="POST" class="formAddPiece ">
+                    <form action="./admin.php" method="POST" class="formAddPiece" enctype="multipart/form-data">
                         <div class="inputs">
-                            <label for="pieceName">Désignation : </label>
-                            <input type="text" name="pieceName" placeholder="Exemple" required>
+                            <label for="userName">Nom : </label>
+                            <input type="text" name="userName" placeholder="Nom de l'utilisateur" required>
                         </div>
                         <div class="inputs">
-                            <label for="pieceRef">Référence : </label>
-                            <input type="text" name="pieceRef" placeholder="ex" required>
+                            <label for="userSurname">Prénom : </label>
+                            <input type="text" name="userSurname" placeholder="Prénom de l'utilisateur" required>
                         </div>
-                        <button type="submit" name="pieceSubmit" value="Add a Piece" class="BtnAdd">
+                        <div class="inputs">
+                            <label for="userEmail">Email : </label>
+                            <input type="text" name="userEmail" placeholder="Email de l'utilisateur" required>
+                        </div>
+                        <div class="inputs">
+                            <label for="userRang">Rang : </label>
+                            <input type="text" name="userRang" placeholder="Admin - Tech - Test" required>
+                        </div>
+                        <div class="inputs">
+                            <label for="userImage">Photo de profil</label>
+                            <input type="file" name="userImage" accept="image/png, image/jpeg" required style="max-width: 200px; margin-left: 1em;">
+                        </div>
+                        <div class="inputs">
+                            <label for="userFormation">Formation : </label>
+                            <input type="text" name="userFormation" placeholder="Laisser vide si pas de formation">
+                        </div><br>
+                        <div class="inputs">
+                            <label for="userLogin">Identifiant : </label>
+                            <input type="text" name="userLogin" placeholder="Identifiant de connexion" required>
+                        </div>
+                        <div class="inputs">
+                            <label for="userPwd">Mot de passe : </label>
+                            <input type="password" name="userPwd" placeholder="Mot de passe de connexion" required>
+                        </div>
+                        <button type="submit" name="userSubmit" value="Add an User" class="BtnAdd">
                             <h2 style="color: #7380ec;">Valider</h2>
                         </button>
                     </form>
                 </section>
-                <?php if (isset($_SESSION['message'])) {
-                    echo $_SESSION['message'];
+                <?php if (isset($_SESSION['messageAdmin'])) {
+                    echo $_SESSION['messageAdmin'];
                 } ?>
             </div>
             <div class=" alert bis">
@@ -123,12 +209,11 @@ $stmt->execute();
                             <th>Technicien</th>
                             <th>Mail</th>
                             <th>Rang</th>
-                            <th>Formation</th>
+                            <th class="formation">Formation</th>
                             <th>Modifier</th>
                             <th>Supprimer</th>
                         </tr>
                     </thead>
-                    <tbody>
                     <tbody>
                         <?php
                         while ($query = $stmt->fetch()) {
@@ -137,14 +222,13 @@ $stmt->execute();
                             echo '<td>' . ucfirst($query['prenom']) . ' ' . strtoupper($query['nom']) . '</td>';
                             echo '<td>' . $query['email'] . '</td>';
                             echo '<td>' . ucfirst($query['rang']) . '</td>';
-                            echo '<td>' . $query['formation'] . '</td>';
+                            echo '<td class="formation">' . $query['formation'] . '</td>';
                             echo '<td class="warning" style="max-width: 100px;"><a href="admin.php?update=true&id=' . $query['id'] . '&nom=' . $query['nom'] . '&prenom=' . $query['prenom'] . '&email=' . $query['email'] . '&rang=' . $query['rang'] . '&formation=' . $query['formation'] . '" style="text-decoration: none; color: #ffbb55; cursor: pointer;"><span class="material-icons-sharp">edit</span></a></td>';
-                            echo '<td class="danger" style="max-width: 100px;"><a href="deleteUser.php?id=' . $query['id'] . '" style="text-decoration: none; color: #ff7782; cursor: pointer;"><span class="material-icons-sharp">delete</span></a></td>';
+                            echo '<td class="danger" style="max-width: 100px;"><a href="deleteUser.php?id=' . $query['id'] . '&nom=' . $query['nom'] . '&prenom=' . $query['prenom'] . '" style="text-decoration: none; color: #ff7782; cursor: pointer;"><span class="material-icons-sharp">delete</span></a></td>';
                             echo '</tr>';
                         } ?>
                     </tbody>
                 </table>
-                <a href="#">Show All</a>
             </div>
         </main>
 
