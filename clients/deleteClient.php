238a76e71e36f84a47262a129b5664147ba4527f
JohanGoutorbe <contact@johan-goutorbe.fr>
2023-05-12 11:01:42 +0200

    Update deleteClient.php

--- clients/deleteClient.php
+++ clients/deleteClient.php
@@ -12,4 +12,37 @@ include '../utils/loggedVerif.php';
 
 $error = '<p style="color: #ff7782;">L\'url est incorrecte</p>';
 
-$location = 'Location: ./pieces.php';
+if (!isset($_GET['id'])) {
+    $_SESSION['message'] = $error;
+    header('Location: ./clients.php');
+    exit();
+} elseif (empty($_GET['id'])) {
+    $_SESSION['message'] = $error;
+    header('Location: ./clients.php');
+    exit();
+} elseif (!ctype_digit($_GET['id'])) {
+    $_SESSION['message'] = $error;
+    header('Location: ./clients.php');
+    exit();
+}
+if (!isset($_GET['name'])) {
+    $_SESSION['message'] = $error;
+    header('Location: ./clients.php');
+    exit();
+} elseif (empty($_GET['name'])) {
+    $_SESSION['message'] = $error;
+    header('Location: ./clients.php');
+    exit();
+}
+
+$id = htmlspecialchars($_GET['id']);
+$name = htmlspecialchars($_GET['name']);
+
+$sql = "DELETE FROM `clients` WHERE `id` = :id";
+$stmt = $db->prepare($sql);
+$stmt->bindParam('id', $id);
+$stmt->execute();
+
+$_SESSION['message'] = '<p style="color: #41f1b6; font-size: 1.25em; font-weight: 100;">La suppression du client <strong>' . $name . '</strong> s\'est bien effectué</p>';
+header('Location: ./clients.php');
+exit();
