3e57e77195d5b337dc8da59f18e0d8a64eeb9ea2
JohanGoutorbe <contact@johan-goutorbe.fr>
2023-05-15 16:59:03 +0200

    Update admin.php

--- admin/admin.php
+++ admin/admin.php
@@ -55,6 +55,42 @@ if (isset($_POST[('userSubmit')])) {
                                     $stmt->bindParam('mdp', $pwd);
                                     $stmt->bindParam('creation_date',$dt);
                                     $stmt->execute();
+
+                                    $file = '../json/logs.json';
+                                    $content = file_get_contents($file);
+                                    $data = json_decode($content, true);
+                                    $maxIndex = 0;
+                                    foreach ($data as $item) {
+                                        $index = intval($item['index']);
+                                        if ($index > $maxIndex
+                                        ) {
+                                            $maxIndex = $index;
+                                        }
+                                    }
+                                    $newIndex = $maxIndex + 1;
+
+                                    $result = $stmt->fetch(PDO::FETCH_ASSOC);
+
+                                    $getdt = new \DateTime();
+                                    $dt = $getdt->format('Y-m-d H:i:s');
+
+                                    $newObject = [
+                                        "logID" => strval($newIndex),
+                                        "type" => "create",
+                                        "userID" => $_SESSION['id'],
+                                        "name" => $_SESSION['name'],
+                                        "surname" => $_SESSION['surname'],
+                                        "role" => $_SESSION['rang'],
+                                        "date" => $dt,
+                                        "action" => "a ajouté l'utilisateur " . ucfirst($prenom) . ' ' . strtoupper($nom) . ' à la liste des utilisateurs'
+                                    ];
+
+                                    array_unshift($data,
+                                        $newObject
+                                    );
+                                    $json = json_encode($data, JSON_PRETTY_PRINT);
+                                    file_put_contents($file, $json);
+                                    
                                     $_SESSION['messageAdmin'] = '<p style="color: #41f1b6; text-shadow: 0px 0px black; width: auto;">L\'utilisateur <strong>' . ucfirst($prenom) . ' ' . strtoupper($nom) . '</strong> a bien été ajouté.</p>';
                                     header("Refresh:0");
                                 } else {
