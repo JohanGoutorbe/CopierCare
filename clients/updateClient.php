b049273e3156135797c5b858271004e240b85cbe
JohanGoutorbe <contact@johan-goutorbe.fr>
2023-05-15 16:59:16 +0200

    Update updateClient.php

--- clients/updateClient.php
+++ clients/updateClient.php
@@ -99,6 +99,39 @@ $stmt->bindParam('adresse', $adresse);
 $stmt->bindParam('interlocuteur', $interlocuteur);
 $stmt->bindParam('id', $id);
 $stmt->execute();
+
+$file = '../json/logs.json';
+$content = file_get_contents($file);
+$data = json_decode($content, true);
+$maxIndex = 0;
+foreach ($data as $item) {
+    $index = intval($item['index']);
+    if ($index > $maxIndex) {
+        $maxIndex = $index;
+    }
+}
+$newIndex = $maxIndex + 1;
+
+$result = $stmt->fetch(PDO::FETCH_ASSOC);
+
+$getdt = new \DateTime();
+$dt = $getdt->format('Y-m-d H:i:s');
+
+$newObject = [
+    "logID" => strval($newIndex),
+    "type" => "update",
+    "userID" => $_SESSION['id'],
+    "name" => $_SESSION['name'],
+    "surname" => $_SESSION['surname'],
+    "role" => $_SESSION['rang'],
+    "date" => $dt,
+    "action" => "a modifié le client " . $name . ' dans la liste des clients'
+];
+
+array_unshift($data, $newObject);
+$json = json_encode($data, JSON_PRETTY_PRINT);
+file_put_contents($file, $json);
+
 $_SESSION['clientMessage'] = '<p style="color: #41f1b6; text-shadow: 0px 0px black; font-size: 1.25em; font-weight: 100;">Le client <strong>' . $name . '</strong> a bien été modifiée.</p>';
 header('Location: ./clients.php');
 exit();
