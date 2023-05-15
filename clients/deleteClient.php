a21582029a0c12fdd845e2c061ef15a5d129d6a9
JohanGoutorbe <contact@johan-goutorbe.fr>
2023-05-15 16:59:11 +0200

    Update deleteClient.php

--- clients/deleteClient.php
+++ clients/deleteClient.php
@@ -43,6 +43,38 @@ $stmt = $db->prepare($sql);
 $stmt->bindParam('id', $id);
 $stmt->execute();
 
+$file = '../json/logs.json';
+$content = file_get_contents($file);
+$data = json_decode($content, true);
+$maxIndex = 0;
+foreach ($data as $item) {
+    $index = intval($item['index']);
+    if ($index > $maxIndex) {
+        $maxIndex = $index;
+    }
+}
+$newIndex = $maxIndex + 1;
+
+$result = $stmt->fetch(PDO::FETCH_ASSOC);
+
+$getdt = new \DateTime();
+$dt = $getdt->format('Y-m-d H:i:s');
+
+$newObject = [
+    "logID" => strval($newIndex),
+    "type" => "delete",
+    "userID" => $_SESSION['id'],
+    "name" => $_SESSION['name'],
+    "surname" => $_SESSION['surname'],
+    "role" => $_SESSION['rang'],
+    "date" => $dt,
+    "action" => "a supprimé le client " . $name . ' de la liste des cliens'
+];
+
+array_unshift($data, $newObject);
+$json = json_encode($data, JSON_PRETTY_PRINT);
+file_put_contents($file, $json);
+
 $_SESSION['message'] = '<p style="color: #41f1b6; font-size: 1.25em; font-weight: 100;">La suppression du client <strong>' . $name . '</strong> s\'est bien effectué</p>';
 header('Location: ./clients.php');
 exit();
