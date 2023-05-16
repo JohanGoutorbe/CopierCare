0a02dd761ed4012dedd53bd5c34a86fb1551ae46
JohanGoutorbe <contact@johan-goutorbe.fr>
2023-05-16 14:57:41 +0200

    Update getImage.php

--- images/getImage.php
+++ images/getImage.php
@@ -1,6 +1,10 @@
 <?php
+error_reporting(0);
+
 include '../utils/dbconnect.php';
+
 include '../utils/loggedVerif.php';
+
 $req=$db->prepare("select * from images where nom=? limit 1");
 $req->setFetchMode(PDO::FETCH_ASSOC);
 $req->execute(array($_GET["nom"]));
