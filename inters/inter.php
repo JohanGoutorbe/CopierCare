d05b5259daa8a2b6395251db13a5124e36b4a97b
JohanGoutorbe <contact@johan-goutorbe.fr>
2023-06-09 15:53:16 +0200

    Update inter.php

--- inters/inter.php
+++ inters/inter.php
@@ -10,6 +10,10 @@ include '../utils/dbconnect.php';
 
 include '../utils/loggedVerif.php';
 
+$sql = "SELECT * FROM `inters` WHERE 1 ORDER BY `id` DESC";
+$stmt = $db->prepare($sql);
+$stmt->execute();
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -89,56 +93,127 @@ include '../utils/loggedVerif.php';
         <main>
             <h1>Interventions</h1>
             <div class="alert">
+                <button id="AddUserBtn" class="AddPieceBtn">
+                    <h2>Ajouter un utilisateur</h2>
+                </button>
+                <section class="sales" id="addPiece">
+                    <form action="./inter.php" method="POST" class="formAddPiece" enctype="multipart/form-data">
+                        <div class="inputs">
+                            <label for="interNumber">Numéro d'inter : </label>
+                            <input type="text" name="interNumber" placeholder="Numéro de l'intervention" required>
+                        </div>
+                        <div class="inputs">
+                            <label for="interDate">Date : </label>
+                            <input type="date" name="interDate" required>
+                        </div>
+                        <div class="inputs">
+                            <label>Client : </label>
+                            <select style="padding:0.25rem; border:1px solid #ccc; border-radius:5px; width:159px;" name="interClient" id="" required>
+                                <option value="" selected disabled hidden>Choisissez un client</option>
+                                <?php
+                                $sentence = "SELECT DISTINCT `id`, `nom_client` FROM `clients` WHERE 1";
+                                $clients = $db->prepare($sentence);
+                                $clients->execute();
+                                while ($client = $clients->fetch()) {
+                                    echo '<option style="padding:0.25rem; border:1px solid #ccc; border-radius:5px; width:159px;" value="' . $client['id'] . '">' . $client['nom_client'] . '</option>';
+                                } ?>
+                            </select>
+                        </div>
+                        <div class="inputs">
+                            <label for="interCopieur">Copieur : </label>
+                            <input type="text" name="interCopieur" placeholder="Copieur lié à l'intervention" required>
+                        </div>
+                        <div class="inputs">
+                            <label for="interTech">Technicien : </label>
+                            <input type="text" name="interTech" placeholder="Technicien de l'intervention" required>
+                        </div>
+                        <div class="inputs">
+                            <label for="interCompteurNB">Compteur NB : </label>
+                            <input type="number" name="interCompteurNB" required>
+                        </div>
+                        <div class="inputs">
+                            <label for="interCompteurCouleur">Compteur Couleur : </label>
+                            <input type="number" name="interCompteurCouleur" required>
+                        </div>
+                        <div class="inputs">
+                            <label for="interPanne">Panne :   </label>
+                            <textarea rows='3' cols='50' name='interPanne' placeholder="Panne du copieur"></textarea>
+                        </div>
+                        <div class="inputs">
+                            <label for="interDiag">Dagnostic :   </label>
+                            <textarea rows='3' cols='50' name='interDiag' placeholder="Diagnostic de la panne"></textarea>
+                        </div>
+                        <div class="inputs">
+                            <label for="interWork">Travaux :   </label>
+                            <textarea rows='3' cols='50' name='interWork' placeholder="Travaux sur le copieur"></textarea>
+                        </div>
+                        <div class="inputs">
+                            <label for="interPieces">Pieces :   </label>
+                            <textarea rows='3' cols='50' name='interPieces' placeholder="Pieces changées du copieur"></textarea>
+                        </div>
+                        <button type="submit" name="userSubmit" value="Add an User" class="BtnAdd">
+                            <h2 style="color: #7380ec;">Valider</h2>
+                        </button>
+                    </form>
+                </section>
+                <?php if (isset($_SESSION['messageAdmin'])) {
+                    echo $_SESSION['messageAdmin'];
+                } ?>
+            </div>
+            <div class="alert bis">
                 <h2>Alertes</h2>
                 <table>
                     <thead>
                         <tr>
-                            <th>Pièce</th>
-                            <th>Copieur</th>
+                            <th style=" display: none;">ID</th>
+                            <th>Numéro d'inter</th>
+                            <th>Date</th>
                             <th>Client</th>
-                            <th>Limittes de la pièce</th>
-                            <th></th>
+                            <th>Copieur</th>
+                            <th>Technicien</th>
+                            <th>Compteur NB</th>
+                            <th>Compteur Couleur</th>
+                            <th>Panne</th>
+                            <th>Diagnostic</th>
+                            <th>Travaux</th>
+                            <th>Pièces changées</th>
                         </tr>
                     </thead>
                     <tbody>
-                        <tr>
-                            <td>Fordable Mini Drone</td>
-                            <td>85631</td>
-                            <td>Due</td>
-                            <td class="warning">Pending</td>
-                            <td class="primary">Details</td>
-                        </tr>
-                        <tr>
-                            <td>Fordable Mini Drone</td>
-                            <td>85631</td>
-                            <td>Due</td>
-                            <td class="warning">Pending</td>
-                            <td class="primary">Details</td>
-                        </tr>
-                        <tr>
-                            <td>Fordable Mini Drone</td>
-                            <td>85631</td>
-                            <td>Due</td>
-                            <td class="warning">Pending</td>
-                            <td class="primary">Details</td>
-                        </tr>
-                        <tr>
-                            <td>Fordable Mini Drone</td>
-                            <td>85631</td>
-                            <td>Due</td>
-                            <td class="warning">Pending</td>
-                            <td class="primary">Details</td>
-                        </tr>
-                        <tr>
-                            <td>Fordable Mini Drone</td>
-                            <td>85631</td>
-                            <td>Due</td>
-                            <td class="warning">Pending</td>
-                            <td class="primary">Details</td>
-                        </tr>
+                        <?php
+                        while ($query = $stmt->fetch()) {
+                            $request = "SELECT `nom_client` FROM `clients` WHERE `id` = :client_id";
+                            $answer = $db->prepare($request);
+                            $answer->execute(['client_id' => $query['client_id']]);
+                            $row = $answer->fetch();
+
+                            $request2 = "SELECT `marque`, `modele` FROM `copieurs` WHERE `id` = :copieur_id";
+                            $answer2 = $db->prepare($request2);
+                            $answer2->execute(['copieur_id' => $query['copieur_id']]);
+                            $row2 = $answer2->fetch();
+
+                            $request3 = "SELECT `nom`, `prenom` FROM `utilisateurs` WHERE `id` = :tech_id";
+                            $answer3 = $db->prepare($request3);
+                            $answer3->execute(['tech_id' => $query['tech_id']]);
+                            $row3 = $answer3->fetch();
+                            echo '<tr>';
+                            echo '<td style="display: none;">' . $query['id'] . '</td>';
+                            echo '<td>' . $query['num_gestco'] . '</td>';
+                            echo '<td>' . $query['date'] . '</td>';
+                            echo '<td>' . $row['nom_client'] . '</td>';
+                            echo '<td>' . ucfirst($row2['marque']) . " " . $row2['modele'] . '</td>';
+                            echo '<td>' . ucfirst($row3['prenom']) . " " . strtoupper($row3['nom']) . '</td>';
+                            echo '<td>' . $query['compteur_nb'] . '</td>';
+                            echo '<td>' . $query['compteur_couleur'] . '</td>';
+                            echo '<td>' . $query['panne'] . '</td>';
+                            echo '<td>' . $query['diagnostic'] . '</td>';
+                            echo '<td>' . $query['travaux'] . '</td>';
+                            echo '<td>' . $query['liste_pieces_changees'] . '</td>';
+                            echo '</tr>';
+                        } ?>
                     </tbody>
                 </table>
-                <a href="#">Show All</a>
+                <a href="#">Voir tout</a>
             </div>
         </main>
         <div class="right">
