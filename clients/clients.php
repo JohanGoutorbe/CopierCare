fc768bcafb4f335daf3e6cc21de991006c012dcb
JohanGoutorbe <contact@johan-goutorbe.fr>
2023-06-27 17:23:50 +0200

    Update clients.php

--- clients/clients.php
+++ clients/clients.php
@@ -37,41 +37,7 @@ if (isset($_POST['clientSubmit'])) {
                                                 $stmt->bindParam('interlocuteur', $interlocuteur);
                                                 $stmt->execute();
 
-                                                $file = '../json/logs.json';
-                                                $content = file_get_contents($file);
-                                                $data = json_decode($content, true);
-                                                $maxIndex = 0;
-                                                foreach ($data as $item) {
-                                                    $index = intval($item['index']);
-                                                    if (
-                                                        $index > $maxIndex
-                                                    ) {
-                                                        $maxIndex = $index;
-                                                    }
-                                                }
-                                                $newIndex = $maxIndex + 1;
-
-                                                $result = $stmt->fetch(PDO::FETCH_ASSOC);
-
-                                                $getdt = new \DateTime();
-                                                $dt = $getdt->format('Y-m-d H:i:s');
-
-                                                $newObject = [
-                                                    "logID" => strval($newIndex),
-                                                    "type" => "create",
-                                                    "userID" => $_SESSION['id'],
-                                                    "name" => $_SESSION['name'],
-                                                    "role" => $_SESSION['rang'],
-                                                    "date" => $dt,
-                                                    "action" => "a ajouté le client " . $name . ' à la liste des clients'
-                                                ];
-
-                                                array_unshift(
-                                                    $data,
-                                                    $newObject
-                                                );
-                                                $json = json_encode($data, JSON_PRETTY_PRINT);
-                                                file_put_contents($file, $json);
+                                                include '../utils/log.php';
 
                                                 $_SESSION['message'] = '<p style="color: #41f1b6; text-shadow: 0px 0px black; width: auto;">Le client <strong>' . $name . '</strong> a bien été ajouté.</p>';
                                                 header("Refresh:0");
