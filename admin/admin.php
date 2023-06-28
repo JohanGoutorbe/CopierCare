4036bd8ce03ae0dab824d7734dcca942959bc232
JohanGoutorbe <contact@johan-goutorbe.fr>
2023-06-28 09:35:35 +0200

    Update admin.php

--- admin/admin.php
+++ admin/admin.php
@@ -50,43 +50,12 @@ if (isset($_POST[('userSubmit')])) {
                                     $stmt->bindParam('formation', $formation);
                                     $stmt->bindParam('identifiant', $login);
                                     $stmt->bindParam('mdp', $pwd);
-                                    $stmt->bindParam('creation_date',$dt);
+                                    $stmt->bindParam('creation_date', $dt);
                                     $stmt->execute();
 
-                                    $file = '../json/logs.json';
-                                    $content = file_get_contents($file);
-                                    $data = json_decode($content, true);
-                                    $maxIndex = 0;
-                                    foreach ($data as $item) {
-                                        $index = intval($item['index']);
-                                        if ($index > $maxIndex
-                                        ) {
-                                            $maxIndex = $index;
-                                        }
-                                    }
-                                    $newIndex = $maxIndex + 1;
-
-                                    $result = $stmt->fetch(PDO::FETCH_ASSOC);
-
-                                    $getdt = new \DateTime();
-                                    $dt = $getdt->format('Y-m-d H:i:s');
-
-                                    $newObject = [
-                                        "logID" => strval($newIndex),
-                                        "type" => "create",
-                                        "userID" => $_SESSION['id'],
-                                        "name" => $_SESSION['name'],
-                                        "surname" => $_SESSION['surname'],
-                                        "role" => $_SESSION['rang'],
-                                        "date" => $dt,
-                                        "action" => "a ajouté l'utilisateur " . ucfirst($prenom) . ' ' . strtoupper($nom) . ' à la liste des utilisateurs'
-                                    ];
-
-                                    array_unshift($data,
-                                        $newObject
-                                    );
-                                    $json = json_encode($data, JSON_PRETTY_PRINT);
-                                    file_put_contents($file, $json);
+                                    $type = "create";
+                                    $action = "a ajouté l'utilisateur " . ucfirst($prenom) . ' ' . strtoupper($nom) . ' à la liste des utilisateurs';
+                                    include '../utils/log.php';
                                     
                                     $_SESSION['messageAdmin'] = '<p style="color: #41f1b6; text-shadow: 0px 0px black; width: auto;">L\'utilisateur <strong>' . ucfirst($prenom) . ' ' . strtoupper($nom) . '</strong> a bien été ajouté.</p>';
                                     header("Refresh:0");
@@ -137,10 +106,10 @@ if (isset($_POST[('userSubmit')])) {
     <div class="container">
         <aside>
             <div class="top">
-                <div class="logo">
+                <a class="logo" href="..\index.php">
                     <img src="../images/getImage.php?nom=logo_copiercare.png" alt="CopierCare logo">
                     <h2><span class="danger">COPIER</span>CARE</h2>
-                </div>
+                </a>
                 <div class="close" id="close-btn">
                     <span class="material-icons-sharp">close</span>
                 </div>
