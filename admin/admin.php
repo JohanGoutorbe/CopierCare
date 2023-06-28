5b19bef81dd0c5c9e0a0cc0a2c51cf12191437df
JohanGoutorbe <contact@johan-goutorbe.fr>
2023-06-28 10:10:29 +0200

    Update admin.php

--- admin/admin.php
+++ admin/admin.php
@@ -106,7 +106,7 @@ if (isset($_POST[('userSubmit')])) {
     <div class="container">
         <aside>
             <div class="top">
-                <a class="logo" href="..\index.php">
+                <a class="logo" href="..\">
                     <img src="../images/getImage.php?nom=logo_copiercare.png" alt="CopierCare logo">
                     <h2><span class="danger">COPIER</span>CARE</h2>
                 </a>
