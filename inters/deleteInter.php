d501f5942959c307cd344bae4935356566b27de9
JohanGoutorbe <contact@johan-goutorbe.fr>
2023-06-30 16:50:47 +0200

    Update deleteInter.php

--- inters/deleteInter.php
+++ inters/deleteInter.php
@@ -9,32 +9,32 @@ include '../utils/loggedVerif.php';
 
 $error = '<p style="color: #ff7782;">L\'url est incorrecte</p>';
 
-$location = 'Location: ./inter.php':
+$location = 'Location: ./inter.php';
 if (!isset($_GET['id'])) {
-    $_SESSION['message'] = $error;
+    $_SESSION['message'] = $error. "1";
     header($location);
     exit();
 } elseif (empty($_GET['id'])) {
-    $_SESSION['message'] = $error;
+    $_SESSION['message'] = $error . "2";
     header($location);
     exit();
 } elseif (!ctype_digit($_GET['id'])) {
-    $_SESSION['message'] = $error;
+    $_SESSION['message'] = $error . "3";
     header($location);
     exit();
 }
-if (!isset($_GET['num_gestco'])) {
-    $_SESSION['message'] = $error;
+if (!isset($_GET['inter'])) {
+    $_SESSION['message'] = $error . "4";
     header($location);
     exit();
-} elseif (empty($_GET['num_gestco'])) {
-    $_SESSION['message'] = $error;
+} elseif (empty($_GET['inter'])) {
+    $_SESSION['message'] = $error . "5";
     header($location);
     exit();
 }
 
 $id = htmlspecialchars($_GET['id']);
-$num_gestco = htmlspecialchars($_GET['num_gestco']);
+$inter = htmlspecialchars($_GET['inter']);
 
 $sql = "DELETE FROM `inter` WHERE `id` = :id";
 $stmt = $db->prepare($sql);
@@ -42,9 +42,9 @@ $stmt->bindParam('id', $id);
 $stmt->execute();
 
 $type = "delete";
-$action = "a supprimé l'intervention " . $num_gestco . ' de la liste des interventions';
+$action = "a supprimé l'intervention " . $inter . ' de la liste des interventions';
 include '../utils/log.php';
 
-$_SESSION['message'] = '<p style="color: #41f1b6; font-size: 1.25em; font-weight: 100;">La suppression de l\'intervention <strong>' . $num_gestco . '</strong> s\'est bien effectuée</p>';
+$_SESSION['message'] = '<p style="color: #41f1b6; font-size: 1.25em; font-weight: 100;">La suppression de l\'intervention <strong>' . $inter . '</strong> s\'est bien effectuée</p>';
 header($location);
 exit();
